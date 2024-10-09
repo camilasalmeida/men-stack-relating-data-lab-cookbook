5ed2c49274df8b7df5471d6c68d5e1febd901998
Camila Alm <camila.salmeidatech@hotmail.com>
2024-10-09 04:50:21 -0400

    create delete funcionality

--- controllers/foods.js
+++ controllers/foods.js
@@ -39,7 +39,18 @@ router.post('/', async (req, res) => {
     }
 });
 
-
+//Delete route
+router.delete('/:foodId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.pantry.id(req.params.foodId).deleteOne();
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/foods`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
 
 module.exports = router;
 
